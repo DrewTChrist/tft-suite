e9e656abb937273d6d72f8b862dcb6aad4e45216
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2021-10-14 07:46:25 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tft-suite",
+    version="0.1.0",
+    author="Andrew Christiansen",
+    author_email="andrewtaylorchristiansen@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drewtchrist",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=[],
+    entry_points={'console_scripts': [],},
+)
